8c5847a33294b8b54dc1947b5ac865c156f6a51e
NikolayLiemiekhov <nicklemster@gmail.com>
2022-03-16 12:29:27 +0200

    Add ellipsis to output string.

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import styled from "styled-components";
 import AdoptedGridRow from "./AdoptedGridRow";
 
 const StyledOutput = styled.div`
   padding: 20px;
+  min-height: 64px;
   background-color: lightgrey;
+  direction: rtl;
+  text-overflow: ellipsis;
+  text-align: left;
+  overflow: hidden;
+  white-space: nowrap;
 `
 
 const Output = (props) => {
