452adf4edc1c29af4bad428fe2532c0cdf26a1ab
NikolayLiemiekhov <nicklemster@gmail.com>
2022-03-17 15:56:00 +0200

    Remove unused variables.

--- src/components/Tile/Styled/StyledBlockBtn.js
+++ src/components/Tile/Styled/StyledBlockBtn.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import {TRANS_300} from "../../../variables";
 
 export const StyledBlockBtn = styled.div`
   position: absolute;
--- src/components/TileList.js
+++ src/components/TileList.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Tile from "./Tile/Tile";
 import styled from "styled-components";
 import AddTileBtn from "./AddTileBtn";
